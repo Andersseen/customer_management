3d3770af0d12e19094b398dffdf3de2913d3ce76
Andrey <forestx24@gmail.com>
2022-06-05 18:44:46 +0200

    added data base

--- src/main/Primary.java
+++ src/main/Primary.java
@@ -2,11 +2,18 @@ package main;
 
 import java.awt.EventQueue;
 
+import conexion.Connect;
 import gui.Window;
 
 public class Primary {
 
 	public static void main(String[] args) {
+		
+		Connect myConnection = new Connect();
+		String response = myConnection.conectar();
+		System.out.println(response);
+		
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
